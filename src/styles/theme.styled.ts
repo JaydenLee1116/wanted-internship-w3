1278440e8c3a308e50e9381bd8e134fa8d3af6e6
JaydenLee1116 <vv55adss@gmail.com>
2023-09-05 21:05:25 +0900

    💄 design: theme style 수정

--- src/styles/theme.styled.ts
+++ src/styles/theme.styled.ts
@@ -2,13 +2,11 @@ import { color } from './color.styled';
 
 export const theme = {
   colors: {
-    white: color.white,
-    gray: {
-      primary: color.gray[600],
-      secondary: color.gray[100],
-    },
-    black: color.black,
     primary: color.blue[600],
-    secondary: color.blue[100],
+    secondary: color.blue[200],
+    white: color.white,
+    black: color.black,
+    darkGray: color.gray[600],
+    lightGray: color.gray[100],
   },
 };

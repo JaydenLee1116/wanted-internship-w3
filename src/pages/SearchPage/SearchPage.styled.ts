5cfe8ac3bf02db383da0755692de7b9ba9324e84
JaydenLee1116 <vv55adss@gmail.com>
2023-09-05 23:21:02 +0900

    ✨ feat: SearchPage 컴포넌트 구현

---
+++ src/pages/SearchPage/SearchPage.styled.ts
@@ -0,0 +1,77 @@
+import { styled } from 'styled-components';
+
+import searchIconGray from '../../assets/icons/search-gray.svg';
+import searchIconWhite from '../../assets/icons/search-white.svg';
+
+export const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.6;
+`;
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h2`
+  font-size: 32px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 24px;
+  width: 600px;
+  height: 72px;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 40px;
+  &:focus-within {
+    box-shadow: 0 0 0 3px ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+export const Input = styled.input`
+  outline: none;
+  width: 70%;
+  height: 70%;
+  border-radius: 40px;
+  font-size: 24px;
+  background-image: url(${searchIconGray});
+  background-repeat: no-repeat;
+  background-position: 0 center;
+  background-size: 40px;
+  padding-left: 48px;
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.darkGray};
+  }
+`;
+
+export const Button = styled.button`
+  outline: none;
+  width: 48px;
+  height: 48px;
+  border-radius: 40px;
+  background-image: url(${searchIconWhite});
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.white};
+  cursor: pointer;
+`;
--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
+import { PageLayout } from '../../components/PageLayout';
+import * as S from './SearchPage.styled';
+
 export const SearchPage = () => {
-  return <div>검색 페이지(메인)입니다.</div>;
+  return (
+    <PageLayout gap={'160px'} backgroundColor="secondary">
+      <S.Header>
+        <S.Title>
+          국내 모든 임상시험 검색하고
+          <br /> 온라인으로 참여하기
+        </S.Title>
+      </S.Header>
+      <S.Main>
+        <S.Form>
+          <S.Input type="text" placeholder="질환명을 입력해 주세요." />
+          <S.Button />
+        </S.Form>
+      </S.Main>
+      <S.Footer>@Wanted Internship By JaydenLee1116</S.Footer>
+    </PageLayout>
+  );
 };

fc23573fcc5a7e0df35ab5d90cfc7e1f2014b253
JaydenLee1116 <vv55adss@gmail.com>
2023-09-06 00:27:28 +0900

    ✨ feat: useInput 커스텀 훅 구현

---
+++ src/hooks/useInput/index.ts
@@ -0,0 +1,9 @@
+import { ChangeEvent, useCallback, useState } from 'react';
+
+export const useInput = <T>(initialValue: T) => {
+  const [value, setValue] = useState<T>(initialValue);
+  const handleInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value as T);
+  }, []);
+  return [value, handleInputChange] as const;
+};

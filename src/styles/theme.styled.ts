a42667ffd0cb1641a384e8202814f2ae87de3a8d
JaydenLee1116 <vv55adss@gmail.com>
2023-09-05 13:13:22 +0900

    💄 design: 글로벌 스타일 적용 및 테마 설정

---
+++ src/styles/theme.styled.ts
@@ -0,0 +1,14 @@
+import { color } from './color.styled';
+
+export const theme = {
+  colors: {
+    white: color.white,
+    gray: {
+      primary: color.gray[600],
+      secondary: color.gray[100],
+    },
+    black: color.black,
+    primary: color.blue[600],
+    secondary: color.blue[100],
+  },
+};

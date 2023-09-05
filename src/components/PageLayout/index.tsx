aa6b1793a6b3c73870529ea2dbd663609d328c68
JaydenLee1116 <vv55adss@gmail.com>
2023-09-06 01:19:03 +0900

    ✨ feat: PageLayout onClick props 추가
    - Layout 컴포넌트를 기준으로 클릭 이벤트를 감지하기 위함

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 
 import * as S from './PageLayout.styled';
 
 interface PageLayoutProps {
   children: React.ReactNode;
+  onClick: (e: MouseEvent<HTMLElement>) => void;
   gap?: string;
   backgroundColor?: 'primary' | 'secondary' | 'black';
 }
 
-export const PageLayout = ({ children, gap, backgroundColor }: PageLayoutProps) => {
+export const PageLayout = ({ children, gap, backgroundColor, onClick }: PageLayoutProps) => {
   return (
-    <S.PageLayoutContainer $gap={gap} $backgroundColor={backgroundColor}>
+    <S.PageLayoutContainer onClick={onClick} $gap={gap} $backgroundColor={backgroundColor}>
       {children}
     </S.PageLayoutContainer>
   );

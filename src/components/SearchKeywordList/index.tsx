d0f73d3e472fd5418b175ecefbb2acd07e3c4f35
JaydenLee1116 <vv55adss@gmail.com>
2023-09-06 23:26:48 +0900

    ✨ feat: hasValue 유틸 적용

--- src/components/SearchKeywordList/index.tsx
+++ src/components/SearchKeywordList/index.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 
 import { SickItem } from '../../types';
+import { hasValue } from '../../utils/hasValue';
 import { SearchKeywordItem } from './SearchKeywordItem';
 import * as S from './SearchKeywordList.styled';
 
 interface SearchKeywordListProps {
-  keywordList: SickItem[];
+  keywordList?: SickItem[] | null;
 }
 
 export const SearchKeywordList = ({ keywordList }: SearchKeywordListProps) => {
+  console.log(hasValue(keywordList));
   return (
     <S.List>
-      {keywordList.map(keywordItem => {
-        return (
-          <SearchKeywordItem key={keywordItem.sickCd} name={keywordItem.sickNm}></SearchKeywordItem>
-        );
-      })}
+      {hasValue(keywordList) &&
+        keywordList?.map(keywordItem => {
+          return (
+            <SearchKeywordItem
+              key={keywordItem.sickCd}
+              name={keywordItem.sickNm}
+            ></SearchKeywordItem>
+          );
+        })}
     </S.List>
   );
 };
